fc0e6ccf58a678308d077c1c0fac593970400934
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2025-03-30 14:25:45 +0530

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,23 @@
+function mergeConfig(nextConfig, userConfig) {
+  if (!userConfig) {
+    return
+  }
+
+  for (const key in userConfig) {
+    if (
+      typeof nextConfig[key] === 'object' &&
+      !Array.isArray(nextConfig[key])
+    ) {
+      nextConfig[key] = {
+        ...nextConfig[key],
+        ...userConfig[key],
+      }
+    } else {
+      nextConfig[key] = userConfig[key]
+    }
+  }
+}
+
 let userConfig = undefined
 try {
   userConfig = await import('./v0-user-next.config')
@@ -23,26 +43,7 @@ const nextConfig = {
   },
 }
 
+// Call mergeConfig *after* defining the function
 mergeConfig(nextConfig, userConfig)
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
-}
 
 export default nextConfig
