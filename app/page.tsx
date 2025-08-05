f415818a70eef260cedf35bf1df31670b1bcf015
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2025-08-05 14:11:51 +0530

    new updates

--- app/page.tsx
+++ app/page.tsx
@@ -168,7 +168,7 @@ export default function Home() {
                       }}
                     >
                       <img
-                        src="https://media-hosting.imagekit.io/63aafe26d12a48c9/Yashvi_ghibli.png?Expires=1837928486&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=KHKwlqVOGEwtm4U6dnBeb74868cO7ZxcdsfnXKg~R4517QB-1qsF8JoVjOfhnGe9B4Ffm7fKfO~QiIyrQ8aQNKSX9X4DJzeV1jB1cAAd~1DW8-aMnAnt9NW-loxe01UNVsKs2ouCzVJrjG7qFhu5voVstVOmidkIhXqgHoJJsnJH2AvbuEmp8M7NjTVHYaU26bA2Xh9zPNJESc9IE-YiiAysW3T7OgR8w6mLFe3XL5Eifw4g~JUlJcdqqjV-TYvwN2Fm8pUKdkv4W5zHGN-8V5ACMdDhTdZNB53rmT5IOPdk2fwppiVyJqeP2NtEzxk-M2N-Vd4CY4wziW-sF-ATnw__"
+                        src="https://ik.imagekit.io/viik8o1ol/db03b8da-1cea-4a9e-a017-627bd9ca0fb2.png?updatedAt=1754382100734"
                         alt="John Doe"
                         className="w-full h-full object-cover"
                       />
--- components/projects-section.tsx
+++ components/projects-section.tsx
@@ -26,7 +26,7 @@ export default function ProjectsSection() {
       description:
         "A React project integrating four APIs for exploring recipes, cocktails, Harry Potter info, and banking details.",
       image:
-        "https://media-hosting.imagekit.io/62748a046db94715/Screenshot%202025-03-28%20144902.png?Expires=1837761617&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=QrTcNWoQSDMhP1grxkvytcAxaVi6ZK9gsRig1ew8rFaJp~MwHCnSwupvifPRI2KJAZCrlkrBkFzE12Fc56mwxiPc99eFcloj9rgxXCQqRQnPs7dqPDa3LmFJvg1ZHiPQqqf8CO5w8lpOzxXub9Od7XRbZg5zH~DfgWo3HyZBzI3TJ1ZEK2itganb4V8Jj1x4yaRHr2al1KjCc6gcuUyWusQe9I-zwCqJS3ZaI8QBddkifY00f~FnNdY2S7YUTDVC6K0AgfQPtpZmC1ErKPhWO71Ipvx5B7g-htc6IB0LsqLlNKvwjdAsRLkjvAVZ4QsSuyY08nSJP~ypkpS7RGnbOw__",
+        "https://ik.imagekit.io/viik8o1ol/Screenshot%202025-03-28%20144902.png?updatedAt=1754382164843",
       tags: ["React", "Axios"],
       demoUrl: "https://api-explorer-react.netlify.app/",
       repoUrl: "https://github.com/yashvi-3106/React-Route",
@@ -36,7 +36,7 @@ export default function ProjectsSection() {
       title: "To-Do List",
       description: "A simple to-do list app with add/delete features and a dark/light mode toggle.",
       image:
-        "https://media-hosting.imagekit.io/2fc6e8696b044b7a/Screenshot%202025-03-28%20152556.png?Expires=1837765862&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=WlWsN5-xnzU5UDd7IUqbC~PEcNfc2XMhZOiNrmJKpxA90DmLjnlyDp8-gQYtNN7OkEdbH7klglD8Y7xzEQX-j146nRcQQizMFQ-ZgW-FePYOnRzFbIkuYuF0cb3cDaROnruwA9VR5LWJeFw3zZK23f9SyTejUsPBEZIntoIkMfMGxcqJD79jKTrzXrhClt41A7W~dDjG2L4BRaXctwxRKsPeceJDigzTkOVZqQmaSvizIw7rLmPFb6qsskPu93e3KdKD71MHBPZrIp-4Fz8JN-wDVdZnn6-HMsNzVq6V3sLz9gaMWw993WaBd6FUl0sNg9waJhNGQ81PbAdL-qvcZA__",
+        "https://ik.imagekit.io/viik8o1ol/Screenshot%202025-03-28%20152556.png?updatedAt=1754382231666",
       tags: ["React"],
       demoUrl: "https://todo-list-react-0.netlify.app/",
       repoUrl: "https://github.com/yashvi-3106/Todo-List-React",
@@ -49,7 +49,7 @@ export default function ProjectsSection() {
       title: "Noble Bids",
       description:
         "A dynamic online bidding system built with the MERN stack (MongoDB, Express.js, React, Node.js). This platform provides a secure and transparent environment for users to participate in live auctions, manage bids, and oversee platform activities.",
-      image: "https://media-hosting.imagekit.io/35b0994e505341e4/Screenshot%202025-03-30%20133705.png?Expires=1837930163&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=YBpJ05QcDJ1R1jYr5q798H5gOFF~pzybAFoSluCt5WwTHsH1TkLhCvmhVHis0CC8jfC3kISeu2n0AjPztGqgUuO91eMMgd4MdCQkZ-B4pFwRHY3pBNt792yoMEkoN08mMvImC-PHTuwjtjfh9jouNLaqXHkxCF1M9AfvZf9VkIQ9Dw2AWDe2pPeiMYdbBuq-dkzLsHP78hbSl9P4U9GMJ~Gp9ync6iXgrSFesayN6AXDDCqhXk2QbfsRqLXXmFo-z~aUpWG7daDg8m7-4JPVDngJNdfczSgK3wcIa6AtDlrWemkZtbLGwsg9FyPd322WHbZ-fax3rDfRHF3IPfDeLw__",
+      image: "https://ik.imagekit.io/viik8o1ol/Screenshot%202025-03-30%20133705.png?updatedAt=1754382340168",
       tags: ["React", "Node.js", "Express", "MongoDB"],
       prUrl: "https://github.com/codinggita/noble_bids/issues/64",
       repoUrl: "https://github.com/codinggita/noble_bids",
@@ -60,7 +60,7 @@ export default function ProjectsSection() {
       title: "Borrowly",
       description:
         "🚀 Borrowly is a web platform that allows users to rent and borrow clothes, fooytwears, and accessories for various occasions. It provides a seamless experience for users to explore, rent, and track their borrowed items.",
-      image: "https://media-hosting.imagekit.io/4e1e217a01be42de/Screenshot%202025-03-30%20133520.png?Expires=1837930043&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=24Bxctfk4oE2GYDQAzXeaeNdsK6QKTzIaIdcH8DI5LhAkUzVqoPP4KoYXuNfNiixBBp4abjizFR4oaT-Oc15FIC-5u9ZfEd--ERDtQDYjdQB3HzN6qRBTVm08~a8o-0UdjEpd2Gpf4XtraaG9WUdPitO~v6hMmw~x1v-bWZ8A3zEpnFTLUk7H64So-u6V6YQDpPbAvDV9hDOkLuFMGw0hnnhcWR9f0qUdNDxKeXvxuDWxraWHeNhP7mUmFtdEhn~ubIp2-ZPJEZKhFRJswzgNk~sVBCJsmGC2RaOuiTBxIexQXqbLLVqK5nebAnL407cozqMaQQk7Jm2gPuJ-tI2hg__",
+      image: "https://ik.imagekit.io/viik8o1ol/Screenshot%202025-03-30%20133520.png?updatedAt=1754382421699",
       tags: ["React", "Node.js", "Express", "MongoDB"],
       prUrl: "https://github.com/codinggita/borrowly/issues/17",
       repoUrl: "https://github.com/codinggita/borrowly",
@@ -72,7 +72,7 @@ export default function ProjectsSection() {
       description:
         "Kofi Hub is a digital platform connecting coffee lovers and small vendors. It simplifies coffee discovery, enables seamless purchases, and fosters a community for enthusiasts. Vendors can showcase their products, gain visibility, and compete with larger brands, while users explore coffee varieties and brewing techniques.",
       image:
-        "https://media-hosting.imagekit.io/8803c74eb75440d8/Screenshot%202025-03-28%20162522.png?Expires=1837767333&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=0KR2~X~4ADtmkptAtwm0Eqvx0Lcf8P5HeWw9ppJiE4AmSS16hC1ovQEcdtN2XLpqH7L73Kr~hkZwZQDzJrDnH9BxVyYK2Sb3AIX3Vq8Y5VHXqOVmBcXMDSkgsyQSStrbgWoS1mIFO-lIvX3K6Kob3Ka3N~xPpBPKtZzfX31SAYJRrYjtD1IJX3K1lHjunDDAQ7BYVWJnxn2chFXOfj3gHfgdLX5bNUei~ys2WrEfSq4V3UNQrEt8jo2UnLcgYSWBUQq2whiGsaOwdC~tLJOjvXhe-Ug5h5VbEJbst5BaJPcW-GWF0RRCO05twp~xh-WIRXPGp7OwInm2eDe4Om1ExQ__",
+        "https://ik.imagekit.io/viik8o1ol/Screenshot%202025-03-28%20162522.png?updatedAt=1754382280141",
       tags: ["React", "Node.js", "Express", "MongoDB"],
       prUrl: "https://github.com/codinggita/kofi_hub/issues/11",
       repoUrl: "https://github.com/codinggita/kofi_hub",
@@ -85,7 +85,7 @@ export default function ProjectsSection() {
       id: 1,
       title: "Freelancer Static Clone",
       description: "A static clone of Freelancer's website focusing on UI replication.",
-      image: "https://media-hosting.imagekit.io/3d91b9c3da284074/Screenshot%202025-03-30%20132703.png?Expires=1837929493&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=nIoMLaxLFwErNGVbw29wbQjHWQlzJflGAovAZULlR2jZIkSXNbF82IYQy7y6hR3XzShFSWH3tnegHtAMqZFDvn1qWjUeskpeoBr3PbkBWQBlhuGUCgy1cxKbxn-Ni2jLlhslVGMd~iBBRFkwZ65PKzg6GtnzTWV5dzw0oJr7XrByK2VBwxjOi8znNeB6Bremotmd2sbgQnIMfRe1UqeZ~LTk7WgJjqvsgaULbxfaWuePHxST0oVtV96Hdjy5zhXpGLPUCSyTuQzTvydoGwfNVzJMd0T3t6MwVOvouxbuy55zbDoBpLQDt0KFjPdvvZ7zVqDbauQYAXgmhsMErrx67A__",
+      image: "https://ik.imagekit.io/viik8o1ol/Screenshot%202025-03-30%20132703.png?updatedAt=1754382490731",
       tags: ["React"],
       demoUrl: "https://freelancer31.netlify.app/",
       repoUrl: "https://github.com/yashvi-3106/Free_lancer-clone",
@@ -94,7 +94,7 @@ export default function ProjectsSection() {
       id: 2,
       title: "Manage Engine Static Clone",
       description: "A static landing page clone of a modern tech company.",
-      image: "https://media-hosting.imagekit.io/cf2d74663a37433e/Screenshot%202025-03-30%20132845.png?Expires=1837929553&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=gnDfwOWoniEwHXTTumAAa6RY8-SQYIFry2ABHmQ2OzHAjA-Ets5S6yJrIY9fnOZjBz4cN2NAAeCjopea2IHuQVrKX3UCgugmIMpvl85jP1Lim5MvxAEzBTCIPi3H5arXTruHekEkJEuCB-uej8vFKKh1F1wlDFwJ9IbaqhXocihAtNs-G6wr8Znwe-2j5DARzd98-eRRDlAVb4j6bNylmBXjuQVcytkTHIrobNwFDML-uZ12DoHGKKUml4gMQmb78O9Q9aMKny7uAiP-tZzDqrtzBzSqAo0ezgPr4-pTrvjZ3EIMaRTlnLJc29Nv1hFjWpx4McSKLVBcLN1iS45hQQ__",
+      image: "https://ik.imagekit.io/viik8o1ol/Screenshot%202025-03-30%20132845.png?updatedAt=1754382511048",
       tags: ["HTML", "CSS"],
       demoUrl: "https://tiny-marigold-a203d3.netlify.app/",
       repoUrl: "https://github.com/yashvi-3106/ManageEngine",
@@ -103,7 +103,7 @@ export default function ProjectsSection() {
       id: 3,
       title: "Naturals Static Clone",
       description: "A static landing page clone of the Naturals Ice Cream website built with HTML & CSS.",
-      image: "https://media-hosting.imagekit.io/9bbdc60a5aa64ab8/Screenshot%202025-03-30%20133030.png?Expires=1837929645&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=NqEhYEd9RI0Vr0spPH2rwZBuGHkvKb1WBeNi5GhKJaZkMaM52HzE5lqd-8pkGvJ0vqtGswbNU1Bhf2L1QLA-NUvO3RWsvcX0xNyWs786~HSFfkVAMdx0hfAojS205yJTW--lc1eo4JxQMoajpp665cbG-9YtZeoNhPb1NQl6nHMQ0kA6xscIuhaSsPV36UUpxqAgCQ53srBnz4uC~6j2dtPHLvSXmdgmuPa5rLxtsStf81nVaMDDoSB~rOg7vQXIhhWo~jGJm96hUQYuFC0OlCD2DCu9xRKwv3arhvsSEZrh14bW8KvvSJc4zhWj7V5rYs82M-srwAgP4fXfuo7G~w__",
+      image: "https://ik.imagekit.io/viik8o1ol/Screenshot%202025-03-30%20133030.png?updatedAt=1754382529678",
       tags: ["HTML", "CSS"],
       demoUrl: "https://naturalsicecream-cg.netlify.app/",
       repoUrl: "https://github.com/yashvi-3106/Naturalsicecream",
